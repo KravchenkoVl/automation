6ce1f1a3d8295dd788dfdc315d5862c7774fb6b5
KravchenkoVl <kravchenko.vl.y@gmail.com>
2019-02-08 18:10:34 +0200

    practice1

--- src/main/java/com/playtika/automation/practice1.java
+++ src/main/java/com/playtika/automation/practice1.java
@@ -16,7 +16,7 @@ public class practice1 {
     }
 
     private static float calculatePrice(float candiesKgPrice, float cookiesKgPrice) {
-        float price1 = (0.3f * candiesKgPrice) + (0.4f * cookiesKgPrice);
-        return price1;
+        float priceM = (0.3f * candiesKgPrice) + (0.4f * cookiesKgPrice);
+        return priceM;
     }
 }
