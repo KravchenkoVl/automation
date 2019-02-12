228a92e811710c9adbd06a74eebbaaa92a9f75c9
KravchenkoVl <kravchenko.vl.y@gmail.com>
2019-02-12 15:41:40 +0200

    1.3

---
+++ src/main/java/com/playtika/automation/HomeWork_1_3.java
@@ -0,0 +1,24 @@
+package com.playtika.automation;
+
+import java.util.Scanner;
+
+public class HomeWork_1_3 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.printf("Введите координату X: ");
+        float x = console.nextFloat();
+        System.out.printf("Введите координату Y: ");
+        float y = console.nextFloat();
+        System.out.printf("Введите координату Z: ");
+        float z = console.nextFloat();
+        calculate(x, y, z);
+    }
+
+    public static void calculate(double x, double y, double z){
+        double length = Math.sqrt(x * x + y * y + z * z);
+        double calcX = x / length;
+        double calcY = y / length;
+        double calcZ = z / length;
+        System.out.printf("Единичный вектор для вектора с координатами (%.0f, %.0f, %.0f) => (%.3f, %.3f, %.3f)", x, y, z, calcX, calcY, calcZ);
+    }
+}
