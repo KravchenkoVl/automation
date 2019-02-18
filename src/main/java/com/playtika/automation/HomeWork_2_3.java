32f911b99f31fe2ee8f4e239b63b84e5c8438f65
KravchenkoVl <kravchenko.vl.y@gmail.com>
2019-02-18 21:26:08 +0200

    add Home Work 2.3

--- src/main/java/com/playtika/automation/HomeWork_2_3.java
+++ src/main/java/com/playtika/automation/HomeWork_2_3.java
@@ -1,7 +1,20 @@
 package com.playtika.automation;
 
+import java.util.Scanner;
+
 public class HomeWork_2_3 {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите количество программистов в компании: ");
+        int i = console.nextInt();
+        System.out.printf("Для найма %d программистов было проведено %d сбеседований", i, sum(i));
+    }
 
+    public static long sum(int n) {
+        if (n <= 1) {
+            return 0;
+        } else {
+            return n - 1 + sum(n - 1);
+        }
     }
 }
